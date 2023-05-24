9770ecef543a30e3af81721497a0f9e1c5e64563
chilleeee1234 <patosachille@gmail.com>
2023-05-24 12:15:33 +0800

    login logout

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\NavigationBarController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
@@ -16,21 +17,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('/login', [AuthController::class, 'login'])->name('user.login');
+Route::post('/user', [UserController::class, 'store'])    ->name('user.store');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+
+    Route::controller(NavigationBarController::class)->group(function(){
+        Route::get('/index',        'index');
+        Route::get('/index/{id}',   'show');
+        Route::post('/index/',      'store');
+        Route::put('/index/{id}',   'update');
+        Route::delete('/index/{id}','destroy');
+    });
+
+    Route::controller(UserController::class)->group(function(){
+        Route::get('/user',                 'index');
+        Route::delete('/user/{id}',         'destroy');
+        Route::get('/user/{id}',            'show');
+        
+        Route::put('/user/{id}',            'update')   ->name('user.update');
+        Route::put('/user/email/{id}',      'email')    ->name('user.email');
+        Route::put('/user/password/{id}',   'password') ->name('user.password');
+    });
+
+   
+
+
 });
 
-Route::get('/index', [NavigationBarController::class, 'index']);
-Route::get('/index/{id}', [NavigationBarController::class, 'show']);
-Route::post('/index/', [NavigationBarController::class, 'store']);
-Route::put('/index/{id}', [NavigationBarController::class, 'update']);
-Route::delete('/index/{id}', [NavigationBarController::class, 'destroy']);
 
-Route::get('/user', [UserController::class, 'index']);
-Route::delete('/user/{id}', [UserController::class, 'destroy']);
-Route::get('/user/{id}', [UserController::class, 'show']);
-Route::post('/user/', [UserController::class, 'store'])->name('user.store');
-Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
-Route::put('/user/email/{id}', [UserController::class, 'email'])->name('user.email');
-Route::put('/user/password/{id}', [UserController::class, 'password'])->name('user.password');
+
+
+// Route::get('/user', [UserController::class, 'index']);
+// Route::delete('/user/{id}', [UserController::class, 'destroy']);
+// Route::get('/user/{id}', [UserController::class, 'show']);
+// Route::post('/user/', [UserController::class, 'store'])->name('user.store');
+// Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
+// Route::put('/user/email/{id}', [UserController::class, 'email'])->name('user.email');
+// Route::put('/user/password/{id}', [UserController::class, 'password'])->name('user.password');
 
